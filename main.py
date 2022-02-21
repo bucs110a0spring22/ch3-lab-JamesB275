6ebcd78b7e7f9010e157d591d69ffeb1b4918073
Jamesb27598 <98533827+JamesB275@users.noreply.github.com>
2022-02-21 23:30:03 +0000

    version 1

--- main.py
+++ main.py
@@ -18,9 +18,35 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. your code goes here
-
+for i in range(10):
+    for speed in [i,i,i,i,i,i,i,i,i,i]:
+      print(michelangelo.forward(speed))
+      print(leonardo.forward(speed))
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
 # Part B - complete part B here
+michelangelo.goto(-70,20)
+michelangelo.down()
+michelangelo.shape
+michelangelo.color('purple')
 
+def drawShape(turd,side):
+  for i in range(side):
+   turd.forward(25)
+   turd.left(360/side)
 
+drawShape(michelangelo,3)
+michelangelo.clear()
+drawShape(michelangelo,4)
+michelangelo.clear()
+drawShape(michelangelo,6)
+michelangelo.clear()
+drawShape(michelangelo,9)
+michelangelo.clear()
+drawShape(michelangelo,12)
+michelangelo.clear()
+
+michelangelo.up()
+michelangelo.goto(-100,20)
 window.exitonclick()
